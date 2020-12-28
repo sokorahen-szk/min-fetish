3b6f35782e0f8f3087dc3b81f3f8be1102078afa
sokorahen-szk <kamonotakahashi@gmail.com>
2020-12-28 12:23:24 +0900

    commit

--- components/Organisms/LeftMenuSlider.vue
+++ components/Organisms/LeftMenuSlider.vue
@@ -38,13 +38,13 @@ interface List {
 @Component
 export default class LeftMenuSlider extends Vue {
   private items: Array<List> = [
-    { id: 1, label: 'テストA', href: '#' },
-    { id: 2, label: 'テストB', href: '#' },
-    { id: 3, label: 'テストC', href: '#' },
-    { id: 4, label: 'テストD', href: '#' },
-    { id: 5, label: 'テストE', href: '#' },
-    { id: 6, label: 'テストF', href: '#' },
-    { id: 7, label: 'テストG', href: '#' },
+    { id: 1, label: 'フェラティオ', href: '#' },
+    { id: 2, label: '中太し', href: '#' },
+    { id: 3, label: '騎乗食', href: '#' },
+    { id: 4, label: 'NTR', href: '#' },
+    { id: 5, label: '窒息プレイ', href: '#' },
+    { id: 6, label: 'コスプレSOX', href: '#' },
+    { id: 7, label: '種着プレス', href: '#' },
     { id: 8, label: 'テストI', href: '#' },
   ];
 
